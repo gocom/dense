bf1d0e49af59d91db01edee31076318fe9159790
Jukka Svahn <web@rahinaa.biz>
2013-06-06 14:35:06 +0300

    Updated tests.

--- test/tests.js
+++ test/tests.js
@@ -4,22 +4,27 @@
 
 (function($)
 {
-    var image = $('<img id="image" src="resources/images/image.jpg" />');
+    var image = $('<img id="image" src="resources/images/image.jpg" />'), loaded = 0;
     $('body').append(image);
     $('#image').dense('updateDimensions');
 
-    var nonExistant = $('<img src="nonexistent.jpg" />').dense('updateDimensions');
+    var nonExistant = $('<img src="nonexistant.jpg" />').dense('updateDimensions').dense();
 
     test("init:chainable", function()
     {
         var image = $('<div />').dense();
-        equal(image.addClass('test').hasClass('test'), true, 'test class found');
+        ok(image.addClass('test').hasClass('test'), 'test class found');
     });
 
     test("updateDimensions:chainable", function()
     {
         var image = $('<div />').dense('updateDimensions');
-        equal(image.addClass('test').hasClass('test'), true, 'test class found');
+        ok(image.addClass('test').hasClass('test'), 'test class found');
+    });
+
+    test("devicePixelRatio", function()
+    {
+        equal($(window).dense('devicePixelRatio') >= 1, true, 'returned device pixel ratio');
     });
 
     $(document).ready(function ()
@@ -32,6 +37,11 @@
         test("updateDimensions:widthNonexistantImage", function()
         {
             equal(nonExistant.attr('width'), undefined, 'image width is undefined');
+        });
+
+        test('init:nonExistantNoChange', function ()
+        {
+            equal(nonExistant.attr('data-jquery-dense-original'), undefined, 'image not changed');
         });
     });
 
